9bbb8a0c730bf1edf76d2edc6e91a553ad623842
Vicente-Ramirez <vramirez2020@alu.uct.cl>
2023-11-23 18:59:20 -0300

    archivo de la aplicacion 1 python

---
+++ aplicacion1.py
@@ -0,0 +1,10 @@
+# 1. Se te encarga la creacion de un proyecto de diseno de antenas destinado a una red inalambrica
+# de alta velocidad. Tu objetivo es crear una antena parabolica que logre concentrar las senales
+# de manera eficiente. La curva que describe la seccion transversal de la antena es f(x) en el
+# intervalo [0, 5] metros, donde x representa la distancia desde el centro de la antena en metros,
+# y f(x) representa la altura en metros. La funcion que modela esta curva es la siguiente:
+# f(x) = 0, 1x
+# 2 + 0, 5x + a, con a el numero de su equipo.
+# a) Representa graficamente la antena parabolica generada al realizar un solido de revolucion 
+# con esta curva a traves de Python
+# b) ¿Cual es el volumen de la antena? Deja expresado en terminos de π.
