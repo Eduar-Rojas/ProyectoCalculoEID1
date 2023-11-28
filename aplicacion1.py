c8352385a637fd55e9cfc08915cf7fa9c8e1fdae
DburgosA <111473775+DburgosA@users.noreply.github.com>
2023-11-27 22:00:54 -0300

    a
    a

--- aplicacion1.py
+++ aplicacion1.py
@@ -1,74 +1,38 @@
-# 1. Se te encarga la creacion de un proyecto de diseno de antenas destinado a una red inalambrica
-# de alta velocidad. Tu objetivo es crear una antena parabolica que logre concentrar las senales
-# de manera eficiente. La curva que describe la seccion transversal de la antena es f(x) en el
-# intervalo [0, 5] metros, donde x representa la distancia desde el centro de la antena en metros,
-# y f(x) representa la altura en metros. La funcion que modela esta curva es la siguiente:
-# f(x) = 0, 1(x) **2 + 0, 5x + a, con a el numero de su equipo.
-# a) Representa graficamente la antena parabolica generada al realizar un solido de revolucion
-# con esta curva a traves de Python
-# b) ¿Cual es el volumen de la antena? Deja expresado en terminos de π.
-
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
+def f(x):
+    return 0.1 * x**2 + 0.5 * x + 0.625
 
-# Definir la función de la curva
-def f(x, a):
-    return 0.1 * x**2 + 0.5 * x + a
+def parabola(x, a, b, c):
+    return a * x**2 + b * x + c
 
+def encontrar_vertice(param):
+    a, b, c = param
+    h = -b / (2 * a)
+    k = parabola(h, a, b, c)
+    return k
 
-# Definir el intervalo [0, 5]
-x_values = np.linspace(0, 5, 100)
+parametros = [0.1, 0.5, 0.625]
+resultado = minimize(encontrar_vertice, parametros)
+a_optimo, b_optimo, c_optimo = resultado.x
 
-# Elegir un valor para 'a'
-a_value = 2
+x = np.linspace(0, 5, 100)
+y = f(x)
 
-# Calcular los valores de la función en el intervalo
-y_values = f(x_values, a_value)
+x_parabola = np.linspace(-5, 7.5, 100)
+y_parabola = parabola(x_parabola, a_optimo, b_optimo, c_optimo)
 
-# Crear la figura y el eje
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
+h_optimo = -b_optimo / (2 * a_optimo)
+k_optimo = parabola(h_optimo, a_optimo, b_optimo, c_optimo)
 
-# Representar la curva 2D
-ax.plot(x_values, y_values, zs=0, zdir="z", label="Curva 2D")
-
-# Generar las coordenadas de la superficie de revolución
-X, Y = np.meshgrid(x_values, y_values)
-Z = np.zeros_like(X)
-
-# Crear la superficie de revolución
-ax.plot_surface(X, Y, Z, color="c", alpha=0.5)
-
-# Configurar etiquetas y título
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Z")
-ax.set_title("Antena Parabólica")
-
-# Mostrar la gráfica
+plt.plot(x_parabola, y_parabola, label='Parábola generada', linestyle='--')
+plt.plot(x, y, label='Curva original')
+plt.scatter(h_optimo, k_optimo, color='red', label='Punto Focal')
+plt.title('Antena Parabólica')
+plt.xlabel('Distancia desde el centro (metros)')
+plt.ylabel('Altura (metros)')
+plt.legend()
+plt.grid(True)
 plt.show()
-
-
-from scipy.integrate import quad
-import numpy as np
-
-
-# Definir la función de la curva
-def f(x, a):
-    return 0.1 * x**2 + 0.5 * x + a
-
-
-# Función para la integración numérica
-def integrando(x, a):
-    return np.pi * f(x, a) ** 2
-
-
-# Elegir un valor para 'a'
-a_value = 2
-
-# Calcular el volumen utilizando la integración numérica
-volumen, error = quad(integrando, 0, 5, args=(a_value,))
-
-# Mostrar el resultado
-print(f"El volumen de la antena es: {volumen}π unidades cúbicas")
